a1648fe7645fb5314cf26e77afb95148cda6cffe
Princ1953e <princ1953e@gmail.com>
2024-05-08 23:53:53 +0530

    PR_1

--- Program/PR_6/PR_6.js
+++
@@ -1,9 +0,0 @@
-let Price = 500;
-
-if (Price >= 100) {
-  console.log("Congratulations..!! You Get 20.Rs Discount");
-} else if (Price >= 50) {
-  console.log("Congratulations..!! You Get 10.Rs Discount");
-} else {
-  console.log("Try Next Time..!!");
-}
